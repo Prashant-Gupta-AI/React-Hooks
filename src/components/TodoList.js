e0a66aeeaeb3599569ea7466a25281c3a3f41444
Prashant <prashantgupta852@gmail.com>
2022-08-07 15:25:21 +0530

    Added TodoList

---
+++ src/components/TodoList.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const TodoList = () =>{
+    const [todos, setTodos] = useState([
+        {text:'Pay bills', id:1},
+        {text:'Do your homework', id:2},
+        {text:'Feed the cat', id:3}
+    ]);
+
+    const addTodo = () =>{
+        setTodos([
+            ...todos,
+            { text:'Learn Hooks', id:Math.random()}
+        ]);
+
+    };
+
+    
+    return(
+        <div>
+            <ul>
+               {todos.map((todo) => {
+                return(
+                    <li key={todo.id}>{todo.text}</li>
+                )
+                })}
+            </ul>
+            <button onClick={addTodo}> Add a Todo</button>
+        </div>
+    )
+};
+
+export default TodoList;
